48f5462b7fa0559d23cdc5a94751b798135ff6a9
Jose Urbina <joseantonioug-09@hotmail.com>
2017-12-08 17:14:26 -0600

    Agregar archivos de proyecto.

---
+++ PracticaEFNET/Models/Direccion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PracticaEFNET.Models
+{
+    public class Direccion
+    {
+        public int CodigoDireccion { get; set; }
+        public string Calle { get; set; }
+    }
+}
---
+++ PracticaEFNET/Models/PracticaDBContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PracticaEFNET.Models
+{
+    public class PracticaDBContext : DbContext
+    {
+        public DbSet<Persona> Persona { get; set; }
+        public DbSet<Direccion> Direccion { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Propiedad de datetime configurar como datetime2
+            modelBuilder.Properties<DateTime>().Configure(x  => x.HasColumnType("datetime2"));
+
+            // Propiedad que inicie con codigo configurar como llave primaria siempre
+            modelBuilder.Properties<int>().Where(x => x.Name.StartsWith("Codigo"))
+                    .Configure(p => p.IsKey());
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
